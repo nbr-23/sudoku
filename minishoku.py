2d8795f1e474f70a05588d1bfa9838cfdfe85f6c
ozdami_b <ozdami_b@etna-alternance.net>
2024-04-13 12:07:35 +0000

    [ADD] generate numbers

--- minishoku.py
+++ minishoku.py
@@ -1,15 +1,5 @@
 import random
-"""
-[0, 0, 0, 0, 0, 0, 0, 0, 0]
-[0, 0, 0, 0, 0, 0, 0, 0, 0]
-[0, 0, 0, 0, 0, 0, 0, 0, 0]
-[0, 0, 0, 0, 0, 0, 0, 0, 0]
-[0, 0, 0, 0, 0, 0, 0, 0, 0]
-[0, 0, 0, 0, 0, 0, 0, 0, 0]
-[0, 0, 0, 0, 0, 0, 0, 0, 0]
-[0, 0, 0, 0, 0, 0, 0, 0, 0]
-[0, 0, 0, 0, 0, 0, 0, 0, 0]
-"""
+
 class Sudoku:
     def __init__(self):
         self.initialize_grid()
@@ -18,20 +8,81 @@ class Sudoku:
     def initialize_grid(self):
         self.grid = [[0 for _ in range(9)] for _ in range(9)]
         
-    # Fist case of the grid and numbers for diagonal 
+    """
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    """
+    # Starting with siagonal subgrids is a common strategy in Sudoku generation algorithms to ensure uniqueness of solution.
     def fill_diagonal_subgrids(self):
-        numbers = list(range(1,10))
         for i in range(0, 9, 3):
-            self.fill_grid(i, i, numbers) # (i,i) is the position where we start to fill the diagonal
+            self.fill_grid(i, i)
 
-def fill_grid(self, row, col, numbers):
-        random.shuffle(numbers)
+    """
+    [4, 2, 9, 0, 0, 0, 0, 0, 0]
+    [8, 6, 5, 0, 0, 0, 0, 0, 0]
+    [7, 1, 3, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 1, 9, 4, 0, 0, 0]
+    [0, 0, 0, 6, 3, 7, 0, 0, 0]
+    [0, 0, 0, 2, 8, 5, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0, 4, 6, 5]
+    [0, 0, 0, 0, 0, 0, 1, 9, 2]
+    [0, 0, 0, 0, 0, 0, 8, 7, 3]
+    """
+    def fill_grid(self, row, col):
+        numbers = random.sample(range(1, 10), 9)
+        index = 0
         for i in range(3):
             for j in range(3):
-                self.grid[row + i][col + j] = numbers.pop(0)
+                self.grid[row + i][col + j] = numbers[index]
+                index += 1
 
+    """
+    [9, 3, 2, 1, 1, 1, 1, 1, 1]
+    [1, 4, 8, 1, 1, 1, 1, 1, 1]
+    [7, 6, 5, 1, 1, 1, 1, 1, 1]
+    [1, 1, 1, 6, 4, 5, 1, 1, 1]
+    [1, 1, 1, 1, 8, 2, 1, 1, 1]
+    [1, 1, 1, 9, 3, 7, 1, 1, 1]
+    [1, 1, 1, 1, 1, 1, 7, 4, 2]
+    [1, 1, 1, 1, 1, 1, 5, 9, 8]
+    [1, 1, 1, 1, 1, 1, 3, 6, 1]
+    """
+    def solve_sudoku(self):
+        for row in range(9):
+            for col in range(9):
+                if self.grid[row][col] == 0:
+                    for num in range(1, 10):
+                        # Function to check if valid number => if valid_number ..
+                        self.grid[row][col] = num
+                        if self.solve_sudoku():
+                            return True
+                        self.grid[row][col] = 0
+                    return False
+        return True
+    
+        
+    # Create a function ro check if the number is valid 
+    # Create a function to remove some random numbers 
+    # Part one done ===
+    
+    #Part two add PyGame
+    
 sudoku = Sudoku()
-for row in sudoku.grid:
-    print(row)
-    
-    
+
+
+solution_found = sudoku.solve_sudoku()
+
+
+if solution_found:
+    print("Sudoku solved ! ")
+    for row in sudoku.grid:
+        print(row)
+else:
+    print("Not solved..")
