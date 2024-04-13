72aace27cc185f53d6aef47a2eaba32682b6f276
ozdami_b <ozdami_b@etna-alternance.net>
2024-04-13 09:49:11 +0000

    [ADD] the diagonal grids filling function

--- minishoku.py
+++ minishoku.py
@@ -1,23 +1,37 @@
-# 1 = implement your game in such a way that it contains a game grid, as well as a generation of numbers on this grid. 
-
+import random
 """
-0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0
+[0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0]
 """
 class Sudoku:
     def __init__(self):
         self.initialize_grid()
+        self.fill_diagonal_subgrids()
 
     def initialize_grid(self):
         self.grid = [[0 for _ in range(9)] for _ in range(9)]
+        
+    # Fist case of the grid and numbers for diagonal 
+    def fill_diagonal_subgrids(self):
+        numbers = list(range(1,10))
+        for i in range(0, 9, 3):
+            self.fill_grid(i, i, numbers) # (i,i) is the position where we start to fill the diagonal
 
-# Example usage
+def fill_grid(self, row, col, numbers):
+        random.shuffle(numbers)
+        for i in range(3):
+            for j in range(3):
+                self.grid[row + i][col + j] = numbers.pop(0)
+
 sudoku = Sudoku()
-print(sudoku.grid)
+for row in sudoku.grid:
+    print(row)
+    
+    
